ead337307322cbec93f85de7dacfabe1363e867a
Polishchyk <polishchyk.a.v@gmail.com>
2024-03-05 03:22:29 +0200

    feat: add transactions info to order table and order details page for admin panel and user account orders page

--- tranzzo_gateway/models/TP_Gateway_Transaction.php
+++ tranzzo_gateway/models/TP_Gateway_Transaction.php
@@ -1,14 +1,26 @@
 <?php
 
 
+/**
+ * Class TP_Gateway_Transaction
+ */
 class TP_Gateway_Transaction {
     private $table_name;
 
+    /**
+     * TP_Gateway_Transaction constructor.
+     */
     public function __construct() {
         global $wpdb;
         $this->table_name = $wpdb->prefix . 'tp_gateway_transactions';
     }
 
+    /**
+     * @param $type
+     * @param $amount
+     * @param $order_id
+     * @return mixed
+     */
     public function create_transaction($type, $amount, $order_id) {
         global $wpdb;
         $wpdb->insert(
@@ -19,9 +31,16 @@ class TP_Gateway_Transaction {
                 'order_id' => $order_id
             )
         );
+
         return $wpdb->insert_id;
     }
 
+    /**
+     * @param $id
+     * @param $type
+     * @param $amount
+     * @param $order_id
+     */
     public function update_transaction($id, $type, $amount, $order_id) {
         global $wpdb;
         $wpdb->update(
@@ -35,6 +54,9 @@ class TP_Gateway_Transaction {
         );
     }
 
+    /**
+     * @param $id
+     */
     public function delete_transaction($id) {
         global $wpdb;
         $wpdb->delete(
@@ -43,51 +65,81 @@ class TP_Gateway_Transaction {
         );
     }
 
+    /**
+     * @param $id
+     * @return mixed
+     */
     public function get_transaction($id) {
         global $wpdb;
+
         return $wpdb->get_row(
             $wpdb->prepare("SELECT * FROM $this->table_name WHERE id = %d", $id),
             ARRAY_A
         );
     }
 
+    /**
+     * @param $order_id
+     * @return mixed
+     */
     public function search_transactions_by_order_id($order_id) {
         global $wpdb;
+
         return $wpdb->get_results(
             $wpdb->prepare("SELECT * FROM $this->table_name WHERE order_id = %d ORDER BY date DESC", $order_id),
             ARRAY_A
         );
     }
 
+    /**
+     * @param $order_id
+     * @return int
+     */
     public function get_total_captured_amount($order_id) {
         global $wpdb;
         $result = $wpdb->get_var(
             $wpdb->prepare("SELECT SUM(amount) FROM $this->table_name WHERE order_id = %d AND type = 'capture'", $order_id)
         );
+
         return $result ? $result : 0;
     }
 
+    /**
+     * @param $order_id
+     * @return int
+     */
     public function get_total_refunded_amount($order_id) {
         global $wpdb;
         $result = $wpdb->get_var(
             $wpdb->prepare("SELECT SUM(amount) FROM $this->table_name WHERE order_id = %d AND type = 'refund'", $order_id)
         );
+
         return $result ? $result : 0;
     }
 
+    /**
+     * @param $order_id
+     * @return int
+     */
     public function get_total_voided_amount($order_id) {
         global $wpdb;
         $result = $wpdb->get_var(
             $wpdb->prepare("SELECT SUM(amount) FROM $this->table_name WHERE order_id = %d AND type = 'void'", $order_id)
         );
+
         return $result ? $result : 0;
     }
 
+    /**
+     * @param $order_id
+     * @return int
+     */
     public function get_total_purchased_amount($order_id) {
         global $wpdb;
         $result = $wpdb->get_var(
             $wpdb->prepare("SELECT SUM(amount) FROM $this->table_name WHERE order_id = %d AND type = 'purchase'", $order_id)
         );
+
         return $result ? $result : 0;
     }
 }
