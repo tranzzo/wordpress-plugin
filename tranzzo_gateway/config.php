064b0da213b8d0334973bfaa45b4d6075a991cf5
Polishchyk <polishchyk.a.v@gmail.com>
2024-04-09 15:14:08 +0300

    feat: add defined variables to config file

--- tranzzo_gateway/config.php
+++ tranzzo_gateway/config.php
@@ -9,3 +9,36 @@ define('TPG_TITLE', 'TRANZZO');
 define('TPG_SUPPORT_EMAIL', 'support@tranzzo.com');
 define('TPG_SUPPORT_URL_UK', 'https://docs.tranzzo.com/uk/');
 define('TPG_SUPPORT_URL_EN', 'https://docs.tranzzo.com/');
+
+define('CUSTOM_PENDING_STATUS_UK', 'https://docs.tranzzo.com/uk/docs/status-codes/payment-cycle/');
+define('CUSTOM_PENDING_STATUS_EN', 'https://docs.tranzzo.com/docs/status-codes/payment-cycle/');
+
+define('CUSTOM_FAILED_STATUS_UK', 'https://docs.tranzzo.com/uk/docs/status-codes/codes-by-stages/');
+define('CUSTOM_FAILED_STATUS_EN', 'https://docs.tranzzo.com/docs/status-codes/codes-by-stages/');
+
+define('CUSTOM_SUCCESS_STATUS_UK', 'https://docs.tranzzo.com/uk/docs/transactions-purchase/overview/');
+define('CUSTOM_SUCCESS_STATUS_EN', 'https://docs.tranzzo.com/docs/transactions-purchase/overview/');
+
+define('CUSTOM_REFUNDED_STATUS_UK', 'https://docs.tranzzo.com/uk/docs/transactions-refund/overview/');
+define('CUSTOM_REFUNDED_STATUS_EN', 'https://docs.tranzzo.com/docs/transactions-refund/overview/');
+
+define('CUSTOM_AUTH_PENDING_STATUS_UK', 'https://docs.tranzzo.com/uk/docs/status-codes/payment-cycle/');
+define('CUSTOM_AUTH_PENDING_STATUS_EN', 'https://docs.tranzzo.com/docs/status-codes/payment-cycle/');
+
+define('CUSTOM_AUTH_FAILED_STATUS_UK', 'https://docs.tranzzo.com/uk/docs/status-codes/codes-by-stages/');
+define('CUSTOM_AUTH_FAILED_STATUS_EN', 'https://docs.tranzzo.com/docs/status-codes/codes-by-stages/');
+
+define('CUSTOM_SUCCESS_AUTH_STATUS_UK', 'https://docs.tranzzo.com/uk/docs/transactions-2-step/auth/');
+define('CUSTOM_SUCCESS_AUTH_STATUS_EN', 'https://docs.tranzzo.com/docs/transactions-2-step/auth/');
+
+define('CUSTOM_AUTH_PART_SUCCESS_STATUS_UK', 'https://docs.tranzzo.com/uk/docs/transactions-2-step/capture/api/#%D0%B7%D0%B0%D1%80%D0%B0%D1%85%D1%83%D0%B2%D0%B0%D0%BD%D0%BD%D1%8F-%D1%87%D0%B0%D1%81%D1%82%D0%B8%D0%BD%D0%B8-%D1%81%D1%83%D0%BC%D0%B8-%D1%80%D0%B5%D0%B7%D0%B5%D1%80%D0%B2%D1%83');
+define('CUSTOM_AUTH_PART_SUCCESS_STATUS_EN', 'https://docs.tranzzo.com/docs/transactions-2-step/capture/api/#capture-the-part-of-the-amount');
+
+define('CUSTOM_AUTH_SUCCESS_STATUS_UK', 'https://docs.tranzzo.com/uk/docs/transactions-2-step/capture/api/#%D0%B7%D0%B0%D1%80%D0%B0%D1%85%D1%83%D0%B2%D0%B0%D0%BD%D0%BD%D1%8F-%D0%B2%D1%81%D1%96%D1%94%D1%97-%D1%81%D1%83%D0%BC%D0%B8-%D1%80%D0%B5%D0%B7%D0%B5%D1%80%D0%B2%D1%83');
+define('CUSTOM_AUTH_SUCCESS_STATUS_EN', 'https://docs.tranzzo.com/docs/transactions-2-step/capture/api/#capture-of-the-entire-amount');
+
+define('CUSTOM_AUTH_VOIDED_STATUS_UK', 'https://docs.tranzzo.com/uk/docs/transactions-2-step/void/');
+define('CUSTOM_AUTH_VOIDED_STATUS_EN', 'https://docs.tranzzo.com/docs/transactions-2-step/void/');
+
+define('CUSTOM_AUTH_REFUNDED_STATUS_UK', 'https://docs.tranzzo.com/uk/docs/transactions-refund/overview/');
+define('CUSTOM_AUTH_REFUNDED_STATUS_EN', 'https://docs.tranzzo.com/docs/transactions-refund/overview/');
